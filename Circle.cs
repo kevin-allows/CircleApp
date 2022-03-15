f730dbe7bc50d1a7384f73af745630235624eca2
Ilya Mankouski <ilya.mankouski@gmail.com>
2022-03-15 13:35:46 +0100

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    internal class Circle
+    {
+        private double circleRadius;
+
+        public void setCircleRadius(double circleRadius)
+        {
+            this.circleRadius = circleRadius;
+        }
+
+        public double getCircleRadius()
+        {
+            return circleRadius;
+        }
+
+        public double getCirclePerimeter()
+        {
+            return 2.0 * Math.PI * circleRadius;
+        }
+
+        public double getCircleArea()
+        {
+            return Math.PI * Math.Pow(circleRadius, 2.0);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Radius = {0}\r\nPerimeter = {1}\r\nArea = {2}\n\r",
+            getCircleRadius(), getCirclePerimeter(), getCircleArea());
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circleFirst = new Circle();
+            Circle circleSecond = new Circle();
+            Circle circleThird = new Circle();
+
+          
+            circleFirst.setCircleRadius(6);
+            Console.WriteLine(circleFirst.getCircleRadius());
+            Console.WriteLine(circleFirst.ToString());
+        
+        }
+    }
+}
